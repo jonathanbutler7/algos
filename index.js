847792392d3b4a204f5919c0199e3a5243baa4b8
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-11-18 12:34:25 -0800

    finished 5. timing our code

---
+++ index.js
@@ -0,0 +1,20 @@
+// 5. timing our code
+
+function addUpTo(n) {
+  let total = 0;
+  for (i = 0; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+addUpTo(100);
+
+function addUpTo(n) {
+  return (n * (n + 1)) / 2;
+}
+
+let t1 = performance.now()
+addUpTo(1000000000)
+let t2 = performance.now()
+console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds`)
