97c4860da2a11b18e48db1bbbd58a22653a8760e
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-11-18 12:43:48 -0800

    finished 7

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ function addUpTo(n) {
   }
   return total;
 }
-
-addUpTo(100);
+// ^ this function has however many operations as n
+// So in this case as and grows the number of operations grow roughly in proportion with n
 
 function addUpTo(n) {
   return (n * (n + 1)) / 2;
 }
+// ^ this function has 3 operations
 
 let t1 = performance.now()
 addUpTo(1000000000)
 let t2 = performance.now()
 console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds`)
+
+// 6. Counting operations
+// it's better to count operations than seconds to compare performance 
