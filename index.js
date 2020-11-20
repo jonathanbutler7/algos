127931f2e9043a5b2c6a7020223f16abd174f7c7
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-11-20 09:21:10 -0800

    added sliding window

--- index.js
+++ index.js
@@ -422,7 +422,52 @@ function countUniqueValues(arr) {
       i++;
       arr[i] = arr[j];
     }
-    console.log(i,j)
+    console.log(i, j);
   }
-  return i + 1
+  return i + 1;
 } // O(n) time
+
+// 5.34 SLIDING WINDOW
+
+// useful when keeping track of a subset of data in a larger set of data
+
+// Write a function called maxSubarraySum which accepts an array of integers and a number called n. the function should calculate the maximum sum of n consecutive elements in the array.
+
+//naive solution:
+
+function maxSubarraySum(arr, num) {
+  if (num > arr.length) {
+    return null;
+  }
+  var max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
+}
+
+maxSubarraySum([1,2,5,2,8,1,5],2) // 10
+maxSubarraySum([4,2,1,6],1) // 6
+
+// sliding window solution: 
+
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum
+}
