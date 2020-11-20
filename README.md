8d6206ab3d22c90c74ec13d40fa2f1524181d96d
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-11-19 22:31:36 -0800

    added 5.18 two pointers

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # udemy-algorithms-course
 
-This file stores the coding exercises I complete while going through Colt Steele's DS&A course on udemy
+This file stores my notes and the coding exercises I complete while going through Colt Steele's DS&A course on udemy
--- index.js
+++ index.js
@@ -371,3 +371,41 @@ function validAnagram(one, two) {
 }
 
 validAnagram('awesome', 'awesome8');
+
+// 5.30
+
+// MULTIPLE POINTERS PROGRAM
+// write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist.
+
+// naive solution
+function sumZero(arr) {
+  for (let i of arr) {
+    for (let j of arr) {
+      if (i + j === 0) {
+        return [i, j];
+      }
+    }
+    return undefined;
+  }
+}
+
+// better solution
+
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+let long = [-3, -2, -1, 0, 1, 2, 3];
+let short = [1, 2, 3];
+sumZero(short);
