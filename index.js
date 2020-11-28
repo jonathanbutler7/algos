746e253612864560f4ba4537ac0253ebe47c9506
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-11-28 11:58:56 -0800

    added optimized selection sort

--- index.js
+++ index.js
@@ -791,17 +791,37 @@ function selectionSort(arr) {
     var lowest = i;
     for (var j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[lowest]) {
-        lowest = j
+        lowest = j;
       }
     }
-    console.log(arr)
-    console.log('swapping!')
+    console.log(arr);
+    console.log('swapping!');
     var temp = arr[i];
     arr[i] = arr[lowest];
     arr[lowest] = temp;
-    console.log(arr)
-  } 
+    console.log(arr);
+  }
+  return arr;
+}
+// optimized
+function selectionSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    var lowest = i;
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      console.log(arr);
+      console.log(i, lowest);
+      var temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+      console.log(arr);
+    }
+  }
   return arr;
 }
 
-selectionSort([34,22,10,19,17,1])
+selectionSort([34, 22, 10, 19, 17, 1]);
