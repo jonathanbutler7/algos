a62580a1e10f12f340f63e29a0e230400f832f8a
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-21 15:06:59 -0800

    added my own sumzero

--- algorithms.js
+++ algorithms.js
@@ -388,9 +388,8 @@ function sumZero(arr) {
   let right = arr.length - 1;
   while (left < right) {
     let sum = arr[left] + arr[right];
-    if (sum === 0) {
-      return [arr[left], arr[right]];
-    } else if (sum > 0) {
+    if (sum === 0) return [arr[left], arr[right]];
+     if (sum > 0) {
       right--;
     } else {
       left++;
