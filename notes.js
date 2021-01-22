9df7670df075c88694f3e4ccdf95984ac59f5894
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-22 11:13:46 -0800

    adds get and set keywords

--- notes.js
+++ notes.js
@@ -9,3 +9,19 @@ const person = {
 };
 
 person.fullName();
+
+// with get and set keywords added
+const person = {
+    first: 'Jonathan',
+    last: 'Butler',
+    get fullName() {
+      return `${person.first} ${person.last}`;
+    },
+    set fullName(value) {
+      const parts = value.split(' ');
+      this.first = parts[0]
+      this.last = parts[1]
+    }
+  };
+  person.fullName = 'tim jenkins'
+  person
