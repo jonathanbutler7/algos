c027b33123f37567ad7f03b0c01025cd81b0b9af
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-31 17:10:59 -0800

    adds links and notes

--- my/mock2.js
+++ my/mock2.js
@@ -1,17 +1,17 @@
-// Give more concrete answers 
+// Give more concrete answers
 
-// Will give you bait but not yank teeth 
+// Will give you bait but not yank teeth
 
-// Give a specific example of problems I solved 
-// You mentioned pages you built. But what does that entail? Did you use boiler plate , build all from scratch. Take out guesswork on his end. 
+// Give a specific example of problems I solved
+// You mentioned pages you built. But what does that entail? Did you use boiler plate , build all from scratch. Take out guesswork on his end.
 
-// Name a specific problem you had to overcome. 
+// Name a specific problem you had to overcome.
 
-// Mention specific feature that changed the game for the client. 
+// Mention specific feature that changed the game for the client.
 
-// Cool pages you built but what was your specific contribution. 
+// Cool pages you built but what was your specific contribution.
 
-// Transition to tech: good answers. Performing under pressures. But link to a specific example. 
+// Transition to tech: good answers. Performing under pressures. But link to a specific example.
 
 // Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
 
@@ -37,9 +37,9 @@ function fourSum(nums, target) {
     }
     if (arr[i] * 4 > target) return quads;
     for (let j = i + 1; j < nums.length - 2; j++) {
-    if (arr[j] === arr[k]) {
-      continue;
-    }
+      if (arr[j] === arr[k]) {
+        continue;
+      }
       let k = i + 2,
         l = nums.length - 1;
       while (k < l) {
@@ -61,37 +61,36 @@ function fourSum(nums, target) {
   return quads;
 }
 
-var fourSum = function(nums, target) {
-    nums.sort((a, b) => a - b);
-    const result = []
-    
-    for(let i = 0; i < nums.length - 3; ++i) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue
-        if (4 * nums[i] > target) break
-        let sum3 = target - nums[i]
-        
-        for(let j = i + 1; j < nums.length - 2; j++) {
-            if (j > i + 1 && nums[j] === nums[j - 1]) continue
-            if (3 * nums[j] > sum3) break
-            
-            let third = j + 1;
-            let fourth = nums.length - 1
+var fourSum = function (nums, target) {
+  nums.sort((a, b) => a - b);
+  const result = [];
 
-            while(third < fourth) {
-                const sum = nums[i] + nums[j] + nums[third] + nums[fourth];
-                if(sum === target) {
-                    result.push([nums[i], nums[j], nums[third], nums[fourth]])
-                    while(nums[third] === nums[third + 1]) third++;
-                    while(nums[fourth] === nums[fourth - 1]) fourth--;
-                    third++;
-                    fourth--;
-                } 
-                else if(sum < target) third++
-              else fourth--
-            }   
-        }   
+  for (let i = 0; i < nums.length - 3; ++i) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    if (4 * nums[i] > target) break;
+    let sum3 = target - nums[i];
+
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+      if (3 * nums[j] > sum3) break;
+
+      let third = j + 1;
+      let fourth = nums.length - 1;
+
+      while (third < fourth) {
+        const sum = nums[i] + nums[j] + nums[third] + nums[fourth];
+        if (sum === target) {
+          result.push([nums[i], nums[j], nums[third], nums[fourth]]);
+          while (nums[third] === nums[third + 1]) third++;
+          while (nums[fourth] === nums[fourth - 1]) fourth--;
+          third++;
+          fourth--;
+        } else if (sum < target) third++;
+        else fourth--;
+      }
     }
-    return result
+  }
+  return result;
 };
 
 // talk aloud even it it's wrong at first.
@@ -100,4 +99,14 @@ var fourSum = function(nums, target) {
 // good i mentioned while loop, similar to two sum, bc i was trying to build off previous idea
 // good to think about first iteration
 // don't look defeated. even if you'e confused, articulate what you're trying
-// don't try to write an optimal solution right off the bat. 
+// don't try to write an optimal solution right off the bat.
+
+// https://leetcode.com/problems/maximal-square/
+// https://leetcode.com/problems/4sum/discuss/504963/javascript-76ms-36.8MB
+// https://leetcode.com/problems/add-two-numbers-ii/
+// https://leetcode.com/problems/candy-crush/
+
+// STAS'S LISTS
+// bloomberg: https://leetcode.com/list/xzqrtueg/
+// favorite: https://leetcode.com/list/xgbjuji1/
+// REACTO strategy: https://www.fullstackacademy.com/blog/whiteboard-coding-interviews-a-6-step-process-to-solve-any-problem
