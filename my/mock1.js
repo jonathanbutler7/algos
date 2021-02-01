261ce1f18e4e759aa1696a8ad1c527f446febeeb
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-31 17:08:26 -0800

    adds mock interview notes

---
+++ my/mock1.js
@@ -0,0 +1,25 @@
+// mock interview:
+
+// Surprised I didn’t ask input and output
+// Practice script for technical interview:
+// What’s input and output?
+// Any edge cases I should be aware of?
+// Valuable to ask if interviewer is concerned about formatting
+
+// If given a fairly simple question, ask if there’s a second question or how much time do I have for this question?
+
+// I spelled indices indeces
+
+// Variable names are important. Need strongly typed language. Typescript for example. Valuable because strongly typed code…would never have hasTripleCharacters var name if it is an array. That sounds like a boolean name. 
+
+// Interviewers don’t wanna be there, keep that in mind. Empathize w the fact that the person may not be 100% there. Empathy part is vital to consider. Understand the interviewer’s frame of mind.
+
+// They have no reason to want you to fail. 
+
+// Pseudo code is your friend. Write one line of pseudocode and then replace it with the code that you just described.
+
+// If I’m not using the iterator I’m creating, I’m possibly doing something wrong.  
+
+// Do you have experience with X? “No but I can learn”
+// Not a great answer to this question. 
+// Ask recruiter ‘hey I’d love to shore up on this, is there anything in particular I should be aware of’
---
+++ my/mock2.js
@@ -0,0 +1,103 @@
+// Give more concrete answers 
+
+// Will give you bait but not yank teeth 
+
+// Give a specific example of problems I solved 
+// You mentioned pages you built. But what does that entail? Did you use boiler plate , build all from scratch. Take out guesswork on his end. 
+
+// Name a specific problem you had to overcome. 
+
+// Mention specific feature that changed the game for the client. 
+
+// Cool pages you built but what was your specific contribution. 
+
+// Transition to tech: good answers. Performing under pressures. But link to a specific example. 
+
+// Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+// Notice that the solution set must not contain duplicate quadruplets.
+
+// Input: nums = [1,0,-1,0,-2,2], target = 0
+// Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+// if no targets can be met, return empty array;
+//
+
+const nums = [1, 0, -1, 0, -2, 2],
+  target = 0;
+
+nums.sort((a, b) => a - b);
+
+function fourSum(nums, target) {
+  // make a var to store a secondary array
+  const quads = [];
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 3; i++) {
+    if (arr[i] === arr[i + 1]) {
+      continue;
+    }
+    if (arr[i] * 4 > target) return quads;
+    for (let j = i + 1; j < nums.length - 2; j++) {
+    if (arr[j] === arr[k]) {
+      continue;
+    }
+      let k = i + 2,
+        l = nums.length - 1;
+      while (k < l) {
+        if (nums[i] + nums[j] + nums[k] + nums[l] === target) {
+          quads.push([nums[i], nums[j], nums[k], nums[l]]);
+          while (nums[k] === nums[k + 1]) k++;
+          while (nums[l] === nums[l - 1]) l--;
+          k++;
+          l--;
+        } else if (sum < target) {
+          k++;
+        } else {
+          l--;
+        }
+      }
+    }
+  }
+  // end loop
+  return quads;
+}
+
+var fourSum = function(nums, target) {
+    nums.sort((a, b) => a - b);
+    const result = []
+    
+    for(let i = 0; i < nums.length - 3; ++i) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue
+        if (4 * nums[i] > target) break
+        let sum3 = target - nums[i]
+        
+        for(let j = i + 1; j < nums.length - 2; j++) {
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue
+            if (3 * nums[j] > sum3) break
+            
+            let third = j + 1;
+            let fourth = nums.length - 1
+
+            while(third < fourth) {
+                const sum = nums[i] + nums[j] + nums[third] + nums[fourth];
+                if(sum === target) {
+                    result.push([nums[i], nums[j], nums[third], nums[fourth]])
+                    while(nums[third] === nums[third + 1]) third++;
+                    while(nums[fourth] === nums[fourth - 1]) fourth--;
+                    third++;
+                    fourth--;
+                } 
+                else if(sum < target) third++
+              else fourth--
+            }   
+        }   
+    }
+    return result
+};
+
+// talk aloud even it it's wrong at first.
+// mention an approach and say why it's not optimal.
+// let the interviewer know you might be able to find a more optimal solution
+// good i mentioned while loop, similar to two sum, bc i was trying to build off previous idea
+// good to think about first iteration
+// don't look defeated. even if you'e confused, articulate what you're trying
+// don't try to write an optimal solution right off the bat. 
