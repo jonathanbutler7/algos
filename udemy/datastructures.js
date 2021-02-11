098b3b23dd34f097636cfb8ba102bd625ca799b4
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-02-10 18:45:30 -0800

    adds BST

--- udemy/datastructures.js
+++ udemy/datastructures.js
@@ -407,3 +407,53 @@ ht.keys().forEach((key) => {
 // - - - check to see if there is a node to the left
 // - - - if there is, move to that node and repeat steps
 // - - - if not, add that node as the left property
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      var current = this.root;
+      while (true) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          }
+          current = current.left;
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          }
+          current = current.right;
+        }
+      }
+    }
+  }
+  search() {}
+}
+
+var tree = new BST(10);
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+tree.insert(10);
+tree.root;
