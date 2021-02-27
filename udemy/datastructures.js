2709e4fda4897b984a6c66d1993ca1d51e49f9ea
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-02-27 12:26:24 -0800

    adds graph intro to /udemy/datastructures

--- udemy/datastructures.js
+++ udemy/datastructures.js
@@ -457,3 +457,48 @@ tree.insert(16);
 tree.insert(7);
 tree.insert(10);
 tree.root;
+
+// graph intro
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    this.adjacencyList[vertex] = [];
+  }
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      console.log(adjacentVertex);
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
+
+const g = new Graph();
+g.addVertex('Tokyo');
+g.addVertex('Dallas');
+g.addVertex('Aspen');
+g.addVertex('HongKong');
+g.addVertex('LosAngeles');
+g.addEdge('Tokyo', 'Dallas');
+g.addEdge('Tokyo', 'HongKong');
+g.addEdge('Dallas', 'Aspen');
+g.addEdge('Dallas', 'HongKong');
+g.addEdge('Dallas', 'LosAngeles');
+g.addEdge('HongKong', 'LosAngeles');
+g.adjacencyList;
