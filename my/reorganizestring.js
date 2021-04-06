1a958ee51e48a39b1f4c349bc89d1726e5957482
Jonathan Butler <jonathan.butler@getweave.com>
2021-04-06 09:49:00 -0700

    adds reorganizestr problem

---
+++ my/reorganizestring.js
@@ -0,0 +1,33 @@
+// leetcode.com/problems/reorganize-string/
+
+https: function reorganizeString(str) {
+  let counter = {};
+  let max = { char: '', frequency: '' };
+  for (char of str) {
+    // frequency counter for each char
+    counter[char] ? counter[char]++ : (counter[char] = 1);
+    // while counting chars, keep track of the most frequent char
+    if (counter[char] > max.frequency) {
+      max.frequency = counter[char];
+      max.char = char;
+    }
+  } // { char: 'b', frequency: 4}
+
+  // if string length is an even number
+  if (str.length % 2 === 0) {
+    // then check if the most frequent char isn't > 1/2 str.length
+    if (max.frequency > str.length / 2) return '';
+  }
+  if (str.length % 2 !== 0) {
+    if (max.frequency > str.length / 2 + 1) return '';
+  }
+  // finds the index of the midpoint of the string
+  const middle = Math.floor(str.length / 2);
+  // takes a substring of the string starting from index 0 and stopping at the midpoint, and splits it into an array
+  const firstHalf = str.substr(0, middle).split('');
+  // takes a substring starting from the midpoint and going to the end of the string, and splits it into an array
+  const secondHalf = str.substr(middle).split('');
+  return { firstHalf, secondHalf };
+}
+
+reorganizeString('aaaaabbbb');
