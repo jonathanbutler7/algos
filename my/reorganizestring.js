8fce652a7a593063ae2364af2e213c643cd5be43
Jonathan Butler <jonathan.butler@getweave.com>
2021-04-07 08:33:53 -0700

    finishes solution

--- my/reorganizestring.js
+++ my/reorganizestring.js
@@ -2,7 +2,7 @@
 
 https: function reorganizeString(str) {
   let counter = {};
-  let max = { char: '', frequency: '' };
+  let max = { char: '', frequency: 0 };
   for (char of str) {
     // frequency counter for each char
     counter[char] ? counter[char]++ : (counter[char] = 1);
@@ -11,8 +11,8 @@ https: function reorganizeString(str) {
       max.frequency = counter[char];
       max.char = char;
     }
-  } // { char: 'b', frequency: 4}
-
+  } // max = { char: 'a', frequency: 5 }
+  
   // if string length is an even number
   if (str.length % 2 === 0) {
     // then check if the most frequent char isn't > 1/2 str.length
@@ -22,12 +22,18 @@ https: function reorganizeString(str) {
     if (max.frequency > str.length / 2 + 1) return '';
   }
   // finds the index of the midpoint of the string
-  const middle = Math.floor(str.length / 2);
+  const middle = (str.length + 1) / 2;
   // takes a substring of the string starting from index 0 and stopping at the midpoint, and splits it into an array
   const firstHalf = str.substr(0, middle).split('');
   // takes a substring starting from the midpoint and going to the end of the string, and splits it into an array
   const secondHalf = str.substr(middle).split('');
-  return { firstHalf, secondHalf };
+  const result = [];
+  // now that the string is sorted and split in two, 
+  for (i in firstHalf) {
+    result.push(firstHalf[i])
+    i < secondHalf.length && result.push(secondHalf[i])
+  }
+  return result;
 }
 
 reorganizeString('aaaaabbbb');
