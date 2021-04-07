4f9c2467bcd10741f5003ad046a535bd25088de1
Jonathan Butler <jonathan.butler@getweave.com>
2021-04-07 08:44:31 -0700

    adds algos group records

--- my/algosstudygroup.js
+++ my/algosstudygroup.js
@@ -1,5 +1,4 @@
 // 2/10
-
 // Implement a function called countUniqueValues, which accepts a sorted array, and counts unique values in the array. There may be negative numbers in the array, but it will always be sorted.
 
 countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
@@ -8,5 +7,16 @@ countUniqueValues([]); // 0
 countUniqueValues([-2, -1, -1, 0, 1]); // 4
 
 // 2/17
-
 // https://leetcode.com/problems/container-with-most-water/
+
+// 3/10
+// https://leetcode.com/problems/course-schedule/
+
+// 3/17 (Brad lead)
+// https://leetcode.com/problems/group-anagrams/
+
+// 3/26
+// https://leetcode.com/problems/reorganize-string/
+
+// 4/2
+// https://leetcode.com/problems/roman-to-integer/
