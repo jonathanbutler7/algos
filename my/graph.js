df60e84bfb7aa3a66efab847ac5d03a544042e38
Jonathan Butler <jonathan.butler@getweave.com>
2021-04-08 08:59:22 -0700

    new notes

--- my/graph.js
+++ my/graph.js
@@ -19,6 +19,7 @@ const adjacencyList = new Map();
 function addNode(airport) {
   adjacencyList.set(airport, []);
 }
+
 // add edge, undirected
 function addEdge(origin, destination) {
   adjacencyList.get(origin).push(destination);
@@ -31,39 +32,39 @@ routes.forEach((route) => addEdge(...route));
 
 console.log(adjacencyList);
 
-function bfs(start) {
-  const visited = new Set();
-  const queue = [start];
-  while (queue.length > 0) {
-    const airport = queue.shift(); //mutates the queue
-    const destinations = adjacencyList.get(airport);
-    for (const destination of destinations) {
-      if (destination === 'BKK') {
-        console.log(`found ${destination}`);
-      }
-      if (!visited.has(destination)) {
-        visited.add(destination);
-        queue.push(destination);
-      }
-    }
-  }
-}
+// function bfs(start) {
+//   const visited = new Set();
+//   const queue = [start];
+//   while (queue.length > 0) {
+//     const airport = queue.shift(); //mutates the queue
+//     const destinations = adjacencyList.get(airport);
+//     for (const destination of destinations) {
+//       if (destination === 'BKK') {
+//         console.log(`found ${destination}`);
+//       }
+//       if (!visited.has(destination)) {
+//         visited.add(destination);
+//         queue.push(destination);
+//       }
+//     }
+//   }
+// }
 
-function dfs(start, visited = new Set()) {
-  console.log(start);
-  visited.add(start);
-  const destinations = adjacencyList.get(start);
-  for (const destination of destinations) {
-    let steps = 0;
-    if (destination === 'BKK') {
-      console.log(`DFS found ${destination} in ${steps} steps`);
-      return;
-    }
-    if (!visited.has(destination)) {
-      steps++;
-      dfs(destination, visited);
-    }
-  }
-}
+// function dfs(start, visited = new Set()) {
+//   console.log(start);
+//   visited.add(start);
+//   const destinations = adjacencyList.get(start);
+//   for (const destination of destinations) {
+//     let steps = 0;
+//     if (destination === 'BKK') {
+//       console.log(`DFS found ${destination} in ${steps} steps`);
+//       return;
+//     }
+//     if (!visited.has(destination)) {
+//       steps++;
+//       dfs(destination, visited);
+//     }
+//   }
+// }
 
-dfs('PHX');
+// dfs('PHX');
